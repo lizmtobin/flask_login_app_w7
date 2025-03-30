34f2f6ae38995ec86508891d5f854cee965d4764
liz tobin <lizmtobin@gmail.com>
2025-03-30 11:39:34 +0100

    add gitignore

--- app/forms.py
+++ app/forms.py
@@ -3,11 +3,13 @@ from wtforms import SubmitField, HiddenField, StringField, PasswordField, Boolea
 from wtforms.fields.numeric import IntegerField
 from wtforms.validators import DataRequired, NumberRange
 
+
 class ChooseForm(FlaskForm):
-    choice = HiddenField('Choice')
+    choice = HiddenField("Choice")
+
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
 
+
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
@@ -12,7 +13,7 @@ class User(UserMixin, db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
     def __repr__(self):
-        return f'User(id={self.id}, username={self.username}, email={self.email})'
+        return f"User(id={self.id}, username={self.username}, email={self.email})"
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -20,7 +21,7 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
-
